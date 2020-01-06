5ab083cf21abef57265f5740b69429683e5f938d
Angel Riera <rriera96@gmail.com>
2020-01-05 22:46:27 -0500

    Adding submit, vue component data and methods property, and wrapping form

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
 <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-      <div class="form-group row">
-          <h1>BearthDay</h1>
-          <div class="col-10">
-              <label for="example-date-input" class="col-2 col-form-label">Date</label>
-              <input class="form-control ml-1" type="date" value="2011-08-19" id="example-date-input">
+      <form v-on:submit.prevent="getImg">
+          <div class="form-group">
+              <h1>BearthDay</h1>
+              <div class="col-3">
+                  <label for="bearthDay" class="col-2 col-form-label">Date</label>
+                  <input class="form-control" type="date" value="" id="bearthDay" v-model="birthday" >
+              </div>
           </div>
-      </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
   </div>
 </template>
 
@@ -17,7 +20,17 @@
 export default {
   name: 'app',
   components: {
-  }
+  },
+    data() {
+        return {
+            birthday: null
+        }
+    },
+    methods: {
+        getImg(){
+            alert(this.birthday);
+        }
+    }
 }
 </script>
 
