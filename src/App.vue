8d35ec82b4e826102bb14e1a7ec51b71cb8b5ebf
Angel Riera <rriera96@gmail.com>
2020-01-05 21:30:53 -0500

    Initial app set up

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+      <div class="form-group row">
+          <h1>BearthDay</h1>
+          <div class="col-10">
+              <label for="example-date-input" class="col-2 col-form-label">Date</label>
+              <input class="form-control ml-1" type="date" value="2011-08-19" id="example-date-input">
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
